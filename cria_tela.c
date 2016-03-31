899668428c09d2f7dd770df024d75c888cc37397
yagolourenco <yago.plv@hotmail.com>
2016-03-31 20:36:56 -0300

    Update cria_tela.c

--- cria_tela.c
+++ cria_tela.c
@@ -1,7 +1,32 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 
-void cria_tela(char *tela){
-  
+int cria_tela(int l,int c,int L, int C){
+char matriz[L][C];
+	for(l=0;l!=L;l++){
+		for(c=0;c!=C;c++){
+			matriz[l][c] = 'o';
 }
+}
+
+	for(l=0;l!=L;l++){
+		for(c=0;c!=C;c++){
+			if((l==0)||(l==L-1)||(c==0)||(c==C-1)){
+				printf("%c ",matriz[l][c]);
+	}else{
+	printf("  ");
+}}
+printf("\n");
+}
+return matriz[l][c];
+}
+
+int main(){
+
+cria_tela(0,0,15,25);
+
+return 0;
+}
+/*IMPRIME A MOLDURA DA MATRIZ NA TELA*/
